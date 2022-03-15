4b37f39fd5e705945c11fc38f36af453d3ca450e
Xiao Yu <jasonyux17@gmail.com>
2022-03-15 16:04:04 -0400

    added Kassim subclassable interface

--- fcassim/FastKassim.py
+++ fcassim/FastKassim.py
@@ -6,27 +6,24 @@ import zipfile
 import os
 import shutil
 import pathlib
-import numpy as np
-import scipy.optimize as su
 
-from nltk.tree import Tree
 from fcassim.ltk.ltk import LabelTreeKernel
 from fcassim.edk.edk import EditDistanceKernel
-from fcassim.cassim.Cassim import Cassim
+from fcassim.cassim.Kassim import Kassim
 
-
-class FastKassim(Cassim):
+class FastKassim(Kassim):
 	LTK = LabelTreeKernel.LTK
 	FTK = LabelTreeKernel.FTK
 	ED = EditDistanceKernel.EDK
 
 	def __init__(self, metric:int, swbd=False, **compute_params):
 		super().__init__(swbd=swbd)
-		self.__kernel = None
+		self.Kernel = None
 		
+		# this also configures kernel
 		metric, compute_params = self.__configure(metric, compute_params)
 		self.__metric = metric
-		self.__params = compute_params
+		self.kernel_params = compute_params
 		return
 
 	@property
@@ -35,25 +32,25 @@ class FastKassim(Cassim):
 
 	@property
 	def params(self):
-		return self.__params
+		return self.kernel_params
 
 	def set_metric(self, value):
-		self.__metric, self.__params = self.__configure(value, self.__params)
+		self.__metric, self.kernel_params = self.__configure(value, self.kernel_params)
 		return
 	
 	def set_params(self, **value):
-		self.__metric, self.__params = self.__configure(self.__metric, value)
+		self.__metric, self.kernel_params = self.__configure(self.__metric, value)
 		return
 
 	def __configure_kernel(self, metric, **params):
 		if self.__metric == FastKassim.LTK:
-			self.__kernel = LabelTreeKernel
+			self.Kernel = LabelTreeKernel
 			return LabelTreeKernel.config(metric, **params)
 		elif self.__metric == FastKassim.FTK:
-			self.__kernel = LabelTreeKernel
+			self.Kernel = LabelTreeKernel
 			return LabelTreeKernel.config(metric, **params)
 		elif self.__metric == FastKassim.ED:
-			self.__kernel = EditDistanceKernel
+			self.Kernel = EditDistanceKernel
 			return EditDistanceKernel.config(metric, **params)
 		return
 
@@ -74,86 +71,8 @@ class FastKassim(Cassim):
 		# configure kernel method call AND its parameters
 		conf_params = self.__configure_kernel(metric, **params)
 
-		logging.info(f"FastKassim Configued kernel={self.__kernel.NAME}, param={conf_params}")
+		logging.info(f"FastKassim Configued kernel={self.Kernel.NAME}, param={conf_params}")
 		return metric, conf_params
-
-	def parse_document(self, doc, tokenizer=None, parser=None):
-		"""parses a document (i.e. a collection of sentences)
-
-		Args:
-			doc (str): a document consisting of one or more sentences
-			tokenizer (Callable, optional): Tokenizer to tokenize the document into sentences. Defaults to self.sent_detector.tokenize.
-			parser (Callable, optional): Parser that can parse a list of sentences into trees. Defaults to self.parser.raw_parse_sents.
-
-		Returns:
-			list: list of parse tree for each sentence in the document
-		"""
-		if tokenizer is None:
-			tokenizer = self.sent_detector.tokenize
-		if parser is None:
-			parser = self.parser.raw_parse_sents
-		
-		parsed_sent = []
-		doc_sents = tokenizer(doc.strip())
-		try:
-			doc_parsed = parser((doc_sents))
-		except Exception as e:
-			print(e)
-			return []
-		doc_parsed = list(doc_parsed)
-		for i in range(len(doc_parsed)):
-			doc_parsed_i = list(doc_parsed[i])[0]
-			parsed_sent_i = Tree.convert(doc_parsed_i)
-			parsed_sent.append(parsed_sent_i)
-		return parsed_sent
-
-	def compute_similarity(self, doc1, doc2):
-		"""compute syntax similarity between two documents
-
-		Args:
-			doc1 (str): document 1 (i.e. a collection of sentences)
-			doc2 (str): document 2 (i.e. a collection of sentences) to compare against
-
-		Returns:
-			float: normalized similarity score
-		"""
-		parsed_doc1 = self.parse_document(doc1)
-		parsed_doc2 = self.parse_document(doc2)
-		return self.compute_similarity_preparsed(parsed_doc1, parsed_doc2)
-
-	def compute_similarity_preparsed(self, parsed_doc1, parsed_doc2):
-		"""compute syntax similarity between two parsed documents
-
-		Args:
-			doc1 (list): list of syntax trees for each sentence in your document 1
-			doc2 (list): list of syntax trees for each sentence in your document 2
-
-		Returns:
-			float: normalized similarity score
-		"""
-		if len(parsed_doc1) == 0 or len(parsed_doc2) == 0:
-			print(f"""
-			Error: Empty document passed in. len(parsed_doc1)={len(parsed_doc1)} and len(parsed_doc2)={len(parsed_doc2)}
-			""")
-			return -1.
-		return self.syntax_similarity_two_parsed_documents(parsed_doc1, parsed_doc2, **self.__params)
-
-	def syntax_similarity_two_parsed_documents(self, doc1_parsed:"list[Tree]", doc2_parsed:"list[Tree]", average=False, **kernal_params): # average=False, sigma=1, lmbda=0.4, use_new_delta=True
-		costMatrix = []
-		for sentencedoc1 in doc1_parsed:
-			temp_costMatrix = []
-			for sentencedoc2 in doc2_parsed:
-				normalized_score = self.__kernel.kernel(sentencedoc1, sentencedoc2, **kernal_params)
-				temp_costMatrix.append(normalized_score)
-			costMatrix.append(temp_costMatrix)
-		costMatrix = np.array(costMatrix)
-		if average==True:
-			return np.mean(costMatrix)
-		else:
-			row_ind, col_ind = su.linear_sum_assignment(costMatrix, True)
-			total = costMatrix[row_ind, col_ind].sum()
-			maxlengraph = max(len(doc1_parsed),len(doc2_parsed))
-			return (total/maxlengraph)
 
 
 def download():
---
+++ fcassim/cassim/Kassim.py
@@ -0,0 +1,108 @@
+__author__ = 'jasonyux'
+import os
+import nltk
+import pathlib
+import numpy as np
+import scipy.optimize as su
+
+from nltk.parse import stanford
+from nltk.tree import Tree
+
+numnodes =0
+root = pathlib.Path(__file__).parent.resolve()
+
+class Kassim:
+	"""
+	Idea from CASSIM: https://github.com/USC-CSSL/CASSIM
+	Combining with kernelized metric, this is now Kernel-assim
+	"""
+
+	def __init__(self, swbd=False, Kernel:object=None, **compute_params):
+		self.sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+		os.environ['STANFORD_PARSER'] = f'{root}/jars/stanford-parser.jar'
+		os.environ['STANFORD_MODELS'] = f'{root}/jars/stanford-parser-3.5.2-models.jar'
+		if swbd == False:
+			self.parser = stanford.StanfordParser(model_path=f"{root}/jars/englishPCFG.ser.gz")
+		else:
+			self.parser = stanford.StanfordParser(model_path=f"{root}/jars/englishPCFG_swbd.ser.gz")
+		
+		self.kernel_params = compute_params
+		self.Kernel = Kernel
+
+	def parse_document(self, doc, tokenizer=None, parser=None):
+		"""parses a document (i.e. a collection of sentences)
+
+		Args:
+			doc (str): a document consisting of one or more sentences
+			tokenizer (Callable, optional): Tokenizer to tokenize the document into sentences. Defaults to self.sent_detector.tokenize.
+			parser (Callable, optional): Parser that can parse a list of sentences into trees. Defaults to self.parser.raw_parse_sents.
+
+		Returns:
+			list: list of parse tree for each sentence in the document
+		"""
+		if tokenizer is None:
+			tokenizer = self.sent_detector.tokenize
+		if parser is None:
+			parser = self.parser.raw_parse_sents
+		
+		parsed_sent = []
+		doc_sents = tokenizer(doc.strip())
+		try:
+			doc_parsed = parser((doc_sents))
+		except Exception as e:
+			print(e)
+			return []
+		doc_parsed = list(doc_parsed)
+		for i in range(len(doc_parsed)):
+			doc_parsed_i = list(doc_parsed[i])[0]
+			parsed_sent_i = Tree.convert(doc_parsed_i)
+			parsed_sent.append(parsed_sent_i)
+		return parsed_sent
+
+	def compute_similarity(self, doc1, doc2):
+		"""compute syntax similarity between two documents
+
+		Args:
+			doc1 (str): document 1 (i.e. a collection of sentences)
+			doc2 (str): document 2 (i.e. a collection of sentences) to compare against
+
+		Returns:
+			float: normalized similarity score
+		"""
+		parsed_doc1 = self.parse_document(doc1)
+		parsed_doc2 = self.parse_document(doc2)
+		return self.compute_similarity_preparsed(parsed_doc1, parsed_doc2)
+
+	def compute_similarity_preparsed(self, parsed_doc1, parsed_doc2):
+		"""compute syntax similarity between two parsed documents
+
+		Args:
+			doc1 (list): list of syntax trees for each sentence in your document 1
+			doc2 (list): list of syntax trees for each sentence in your document 2
+
+		Returns:
+			float: normalized similarity score
+		"""
+		if len(parsed_doc1) == 0 or len(parsed_doc2) == 0:
+			print(f"""
+			Error: Empty document passed in. len(parsed_doc1)={len(parsed_doc1)} and len(parsed_doc2)={len(parsed_doc2)}
+			""")
+			return -1.
+		return self.syntax_similarity_two_parsed_documents(parsed_doc1, parsed_doc2, **self.kernel_params)
+
+	def syntax_similarity_two_parsed_documents(self, doc1_parsed:"list[Tree]", doc2_parsed:"list[Tree]", average=False, **kernal_params):
+		costMatrix = []
+		for sentencedoc1 in doc1_parsed:
+			temp_costMatrix = []
+			for sentencedoc2 in doc2_parsed:
+				normalized_score = self.Kernel.kernel(sentencedoc1, sentencedoc2, **kernal_params)
+				temp_costMatrix.append(normalized_score)
+			costMatrix.append(temp_costMatrix)
+		costMatrix = np.array(costMatrix)
+		if average==True:
+			return np.mean(costMatrix)
+		else:
+			row_ind, col_ind = su.linear_sum_assignment(costMatrix, True)
+			total = costMatrix[row_ind, col_ind].sum()
+			maxlengraph = max(len(doc1_parsed),len(doc2_parsed))
+			return (total/maxlengraph)
